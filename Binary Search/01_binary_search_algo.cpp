03ec91d0debb998a66b1a25e41c23bd1d5ae9ca0
Sumit Kumar Das <73597446+sumit-kr-das@users.noreply.github.com>
2022-12-04 18:06:30 +0530

    new problem added

--- Binary Search/01_binary_search_algo.cpp
+++ Binary Search/01_binary_search_algo.cpp
@@ -1,24 +1,18 @@
-#include<iostream>
-using namespace std;
-
-int binarySearch(int arr[], int size, int ele){
-    int start = 0;
-    int end = size;
-    while(start<=end){
-        int mid = (start+end)/2;
-        if(ele == arr[mid]){
-            return mid;
-        }else if(ele<arr[mid]){
-            end = mid-1;
-        }else{
-            start = mid+1;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size()-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target == nums[mid]){
+                return mid;
+            }else if(target<nums[mid]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
         }
+        return -1;
     }
-    return -1;
-}
-
-int main(){
-    int arr[]={1,2,3,4,5,6,7};
-    cout<<binarySearch(arr,7,6);
-    return 0;
-}
+};
